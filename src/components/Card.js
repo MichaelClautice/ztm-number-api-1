91612f8c039b4d59cf4fd9e16e674a840495b1ca
Efe Erhie <efe_erhie@git_commits.me>
2022-04-05 22:12:04 -0500

    Made the card sizes more responsive to screen sizes

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 class Card extends Component{
   render(){
     return (
-    <div className='tc card br3 ma2 pa3 grow bw2 shadow-5 dib w-third'>
+    <div className='tc card br3 ma2 pa3 grow bw2 shadow-5 dib w-80-ns w-40-m w-20-l'>
       <img src={`https://avatars.dicebear.com/api/personas/${this.props.number}.svg`} alt={`A dice bear saying ${this.props.number}`} />
       <div>
         <h2>A {this.props.type} fact about {this.props.number} is </h2>
